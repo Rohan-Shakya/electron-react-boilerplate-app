749725fbeaaa4686486efa59340f03324aa8a190
Rohan-Shakya <rohanshakya254@gmail.com>
2021-09-09 15:27:13 +0545

    links updated added scss

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.global.css';
+import icon from '../../assets/icon.png';
+import './App.global.scss';
 
 const Hello = () => {
   return (
@@ -9,7 +9,7 @@ const Hello = () => {
       <div className="Hello">
         <img width="200px" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
+      <h1>Electron-React-Boilerplate-App</h1>
       <div className="Hello">
         <a
           href="https://electron-react-boilerplate.js.org/"
@@ -20,19 +20,7 @@ const Hello = () => {
             <span role="img" aria-label="books">
               📚
             </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
+            Get Started
           </button>
         </a>
       </div>
